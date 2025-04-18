5cf71d636480d521ba15d8d79dfd2a27b27c2e02
Tracey Lum <traceylum@gmail.com>
2025-04-17 19:40:24 -0700

    update learning article

--- src/app/routes/Learning/Learning.js
+++ src/app/routes/Learning/Learning.js
@@ -7,25 +7,24 @@ function Learning() {
       <Header/>
       <div className="Learning">
       <article>
-        <h2>4-3-2025</h2>
+        <h2>4-17-2025</h2>
         <section>
           <p>
-            I started my CS learning journey after I came back from living and working abroad in Taiwan for 6 years. I worked as an English teacher, which I enjoyed, but knew it wasn't my passion.
+            I started my CS learning journey after I came back from living and working abroad in Taiwan for 6 years. I worked as an English teacher and editor during my time there, and while I enjoyed both jobs and learned a lot, I knew that neither was my passion.
           </p>
           <p>
-            I decided that I wanted to challenge myself and pursue career goals that would push me beyond what I had ever done. Being a person who was always more involved in extra-curriculars and athletics rather than education, my studies have taken a backseat for most of my life. 
+            After spending several years working in the ESL field, I knew that if I wanted to change my trajectory, I would have to do it soon. So with the hopes of giving myself a fresh new direction in life, I listed all of my interests and skillsets. I analyzed and picked apart each one, but there was nothing that stood out which felt like it could lead to an exciting career path. There was, however, a nagging feeling that had always been at the back of my mind since youth... It was the belief that I was not someone who was 'textbook smart.'
           </p>
           <p>
-            I participated in the education system enough to pass exams and get by with decent grades, but never truly understood what it meant to <i>learn</i> something.
+            During my time in the formal education system, I was more involved in extra-curriculars and athletics, and being a student had taken a backseat. I decided that I wanted to challenge this limiting belief of myself and explore fields that would push me beyond what I had ever done intellectually. This time I created a new list--a list of subjects that had always felt overwhelming, intimidating, and simply impossible for me to excel at. Of those at the top of the list, I weighed the subjects which felt most interesting and felt like I would enjoy diving deeper into, and I ended up choosing Computer Science.
           </p>
           <p>
-            Now I have a new life goal: To be a life-long student.
+            Once I made my decision, I packed my bags and left my comfortable lifestyle in Asia to do what I had never done before: to become a real student and dive headfirst into learning.
           </p>
         </section>
         </article>
       </div>
     </>
-
   );
 }
 
